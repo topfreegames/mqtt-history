eb1dc4bbb18106d5ef61b852c0a23c457c3c4d70
mflilian <mayhharamorais@gmail.com>
2022-04-11 21:39:25 -0300

    adds new boolean variable to the function

--- app/query_param.go
+++ app/query_param.go
@@ -8,10 +8,11 @@ import (
 	"github.com/labstack/echo"
 )
 
-func ParseHistoryQueryParams(c echo.Context, defaultLimit int64) (string, int64, int64) {
+func ParseHistoryQueryParams(c echo.Context, defaultLimit int64) (string, int64, int64, bool) {
 	userID := c.QueryParam("userid")
 	from, _ := strconv.ParseInt(c.QueryParam("from"), 10, 64)
 	limit, _ := strconv.ParseInt(c.QueryParam("limit"), 10, 64)
+	retrieve, _ := strconv.ParseBool(c.QueryParam("retrieve"))
 
 	if limit == 0 {
 		limit = defaultLimit
@@ -21,7 +22,7 @@ func ParseHistoryQueryParams(c echo.Context, defaultLimit int64) (string, int64,
 		from = time.Now().Unix()
 	}
 
-	return userID, from, limit
+	return userID, from, limit, retrieve
 }
 
 func ParseHistoriesQueryParams(c echo.Context, defaultLimit int64) ([]string, string, int64, int64) {
