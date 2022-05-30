b524efdeaa5c8198357f07595ed5108bb0f200af
mflilian <mayhharamorais@gmail.com>
2022-05-30 19:52:31 -0300

    creates new handler to use on player support endpoint

---
+++ app/histories_v2_player_support.go
@@ -0,0 +1,56 @@
+package app
+
+import (
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/topfreegames/mqtt-history/logger"
+	"github.com/topfreegames/mqtt-history/mongoclient"
+
+	"github.com/labstack/echo"
+	"github.com/topfreegames/mqtt-history/models"
+)
+
+func HistoriesV2PSHandler(app *App) func(c echo.Context) error {
+	return func(c echo.Context) error {
+		c.Set("route", "HistoriesV2PlayerSupport")
+		//topic := c.ParamValues()[0]
+
+		userID, _, limit, isBlocked := ParseHistoryQueryParams(c, app.Defaults.LimitOfMessages)
+		authenticated, _, err := IsAuthorized(c.StdContext(), app, userID)
+		if err != nil {
+			return err
+		}
+
+		initialDateParamsFilter := c.QueryParam("initialDate")
+		finalDateParamsFilter := c.QueryParam("finalDate")
+		from := transformDate(initialDateParamsFilter)
+		to := transformDate(finalDateParamsFilter)
+
+		logger.Logger.Debugf(
+			"user %s (authenticated=%v) is asking for history v2 with args from=%d to=%d and limit=%d",
+			userID, authenticated, from, to, limit)
+
+		if !authenticated {
+			return c.String(echo.ErrUnauthorized.Code, echo.ErrUnauthorized.Message)
+		}
+
+		messages := make([]*models.MessageV2, 0)
+		collection := app.Defaults.MongoMessagesCollection
+		messages = mongoclient.GetMessagesPlayerSupportV2WithParameter(c, from, to, limit, collection, isBlocked)
+
+		return c.JSON(http.StatusOK, messages)
+	}
+}
+
+func transformDate(dateParamsFilter string) int64 {
+
+	res := strings.Split(dateParamsFilter, "/")
+	day, _ := strconv.Atoi(res[0])
+	month, _ := strconv.Atoi(res[1])
+	year, _ := strconv.Atoi(res[2])
+
+	return time.Date(year, time.Month(month), day, 23, 59, 59, 59, time.UTC).Unix()
+}
