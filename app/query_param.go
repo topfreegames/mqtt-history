13564c219f537eaf68a2d5e0f2ca43ba8a12dba4
mflilian <mayhharamorais@gmail.com>
2022-06-03 12:22:07 -0300

    creates new function to handle query params

--- app/query_param.go
+++ app/query_param.go
@@ -9,7 +9,47 @@ import (
 	"github.com/topfreegames/mqtt-history/logger"
 )
 
-func ParseHistoryQueryParams(c echo.Context, defaultLimit int64) (string, string, string, int64, int64, int64, bool) {
+func ParseHistoryQueryParams(c echo.Context, defaultLimit int64) (string, int64, int64, bool) {
+	userID := c.QueryParam("userid")
+	from, _ := strconv.ParseInt(c.QueryParam("from"), 10, 64)
+	limit, _ := strconv.ParseInt(c.QueryParam("limit"), 10, 64)
+	isBlocked, err := strconv.ParseBool(c.QueryParam("isBlocked"))
+
+	if limit == 0 {
+		limit = defaultLimit
+	}
+
+	if from == 0 {
+		from = time.Now().Unix()
+	}
+
+	if err != nil {
+		// If it returns error, it will assume the default behavior(e.g. isBlocked=false).
+		logger.Logger.Warningf("Error getting isBlocked parameter, assuming default behavior. Error: %s", err.Error())
+		return userID, from, limit, false
+	}
+
+	return userID, from, limit, isBlocked
+}
+
+func ParseHistoriesQueryParams(c echo.Context, defaultLimit int64) ([]string, string, int64, int64) {
+	userID := c.QueryParam("userid")
+	topicsSuffix := strings.Split(c.QueryParam("topics"), ",")
+	from, _ := strconv.ParseInt(c.QueryParam("from"), 10, 64)
+	limit, _ := strconv.ParseInt(c.QueryParam("limit"), 10, 64)
+
+	if limit == 0 {
+		limit = defaultLimit
+	}
+
+	if from == 0 {
+		from = time.Now().Unix()
+	}
+
+	return topicsSuffix, userID, from, limit
+}
+
+func ParseHistoryPSQueryParams(c echo.Context, defaultLimit int64) (string, string, string, int64, int64, int64, bool) {
 	userID := c.QueryParam("userid")
 	playerId := c.QueryParam("playerId")
 	topic := c.QueryParam("topic")
@@ -33,21 +73,4 @@ func ParseHistoryQueryParams(c echo.Context, defaultLimit int64) (string, string
 	}
 
 	return userID, playerId, topic, from, to, limit, isBlocked
-}
-
-func ParseHistoriesQueryParams(c echo.Context, defaultLimit int64) ([]string, string, int64, int64) {
-	userID := c.QueryParam("userid")
-	topicsSuffix := strings.Split(c.QueryParam("topics"), ",")
-	from, _ := strconv.ParseInt(c.QueryParam("from"), 10, 64)
-	limit, _ := strconv.ParseInt(c.QueryParam("limit"), 10, 64)
-
-	if limit == 0 {
-		limit = defaultLimit
-	}
-
-	if from == 0 {
-		from = time.Now().Unix()
-	}
-
-	return topicsSuffix, userID, from, limit
 }
