326e547df9cceddfc0c0b4a67e1e19603fc1236f
mflilian <mayhharamorais@gmail.com>
2022-06-06 17:15:41 -0300

    add test case for no player id being passed

--- app/history_v2_player_support_test.go
+++ app/history_v2_player_support_test.go
@@ -72,6 +72,28 @@ func TestHistoryV2PSHandler(t *testing.T) {
 
 			})
 
+			g.It("It should return 200 and unblocked messages from the given topic and date.", func() {
+				testID := strings.Replace(uuid.NewV4().String(), "-", "", -1)
+				topic := fmt.Sprintf("chat/test_%s", testID)
+				finalDate := strings.Split(time.Now().AddDate(0, 0, 1).UTC().String(), " ")[0]
+
+				err := InsertMongoMessagesWithParameters(ctx, []string{topic}, false)
+				Expect(err).To(BeNil())
+
+				path := fmt.Sprintf("/ps/v2/history?topic=%s&initialDate=2022-01-01&finalDate=%s", topic, finalDate)
+				status, body := Get(a, path, t)
+				g.Assert(status).Equal(http.StatusOK)
+
+				var messages []models.MessageV2
+
+				err = json.Unmarshal([]byte(body), &messages)
+				Expect(err).To(BeNil())
+
+				g.Assert(len(messages)).Equal(1)
+				g.Assert(messages[0].Blocked).Equal(false)
+
+			})
+
 			g.It("It should return 422 and an error message if the date parameter is being missed", func() {
 				testID := strings.Replace(uuid.NewV4().String(), "-", "", -1)
 				topic := fmt.Sprintf("chat/test_%s", testID)
